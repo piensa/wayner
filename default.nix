8e2158ddb311f3d646d1a425d89537a024d7eac2
Ariel <ingenieroariel@gmail.com>
2019-04-28 18:53:08 -0500

    Added path to fetchgit

--- default.nix
+++ default.nix
@@ -51,7 +51,7 @@ waylandPkgs = rec {
     name = "blender-2.80.beta-${version}";
     version = "141c6073ca39";
 
-    src = fetchgit {
+    src = pkgs.fetchgit {
       url = "https://git.blender.org/blender.git";
       rev = version;
       sha256 = "0g02y05hr5vim5g6myy16c1dvx6ah8xbjd1j5jw62qd10yfg4kfp";
